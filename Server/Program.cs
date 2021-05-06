4c36b9d99c4af80b63f8231217e62b54bc8833e7
phyzg0d <mephyz@mail.ru>
2021-05-06 15:40:41 +0300

    add: server core

---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using DefaultNamespace;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new BaseServer(80);
+        }
+    }
+}
